596817ab72295e2c20e2c795c0e5b68371b7f099
Carlos Carneiro <mcarlospc@gmail.com>
2022-04-27 12:03:29 -0300

    Code changes

--- calculations/basic.js
+++ calculations/basic.js
@@ -3,8 +3,8 @@
     Meaning it is a measure of how far a set of numbers is spread out from their mean.
 
     examples:
-    variance of 8, 10, 13 = 4.222222222222222 // returns 4
-    variance of 1, 33, 50 = 412.6666666666667 // returns 412
+    variance of 8, 10, 13 = 4.222222222222222
+    variance of 1, 33, 50 = 412.6666666666667
 */
 export function variance(inputs) {
     if (inputs) {
@@ -17,7 +17,7 @@ export function variance(inputs) {
         let sum = arr.reduce((acc, curr)=> acc + curr, 0);
         let variance = sum / arr.length
 
-		return Math.trunc(variance)
+		return variance
     }
 
     throw 'Failed to calculate variance'
@@ -32,7 +32,7 @@ export function variance(inputs) {
 */
 export function mean(inputs) {
     if (inputs) {
-        return inputs.reduce((a, b) => a + b) / inputs.length;
+        return -777;
     }
 
     throw 'Failed to calculate mean'
--- test/calcs.spec.js
+++ test/calcs.spec.js
@@ -6,14 +6,21 @@ chai.use(chaiHttp);
 let should = chai.should();
 
 describe('/GET', () => {
-    it('it should return \'Hello World!\'', (done) => {
+    it('it should return \'Hello World!\' with status 200', (done) => {
       chai.request(app)
           .get('/')
           .end((err, res) => {
                 res.should.have.status(200);
-                res.text.should.be.a('string');
                 res.text.should.be.equal("Hello World!");
             done();
           });
     });
 });
+
+// Test Variance
+// test /calculations/variance/{inputs} with invalid inputs -- Should return status 400 with string message 'error parsing inputs'
+// test /calculations/variance/{inputs} with valid inputs -- Should return status 200 with body {"result":{result}}
+
+// Test Mean
+// test /calculations/mean/{inputs} with invalid inputs -- Should return status 400 with string message 'error parsing inputs'
+// test /calculations/mean/{inputs} with valid inputs -- Should return status 200 with body {"result":{result}}
